8f5a3ee4ed4039f14583b8e2f437941b837b7642
redheart76 <redheart76@gmail.com>
2021-12-19 01:30:10 +1300

    Initial files done

---
+++ Mars_Onboard_Task-1-2/Mars_Onboard_Task-1-2/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Mars_Onboard_Task_1_2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
